a2ef51a89d77622fbc801fc30c05de3931fb7806
Minit-WangMaixin <3073486251@qq.com>
2022-10-02 11:27:54 +0800

    deploy

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    themeConfig: {
+      lastUpdated: '更新时间',
+      logo: '/assets/img/logo.png',
+      nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/about/' },
+
+          {
+            text: 'Languages',
+            items: [
+              { text: 'Group1', items: [
+                { text: 'Home', link: '/' },
+                { text: 'Guide', link: '/about/' },
+              ] },
+
+              { text: 'Group2', items: [
+                { text: 'Home', link: '/' },
+                { text: 'Guide', link: '/about/' },
+              ] }
+            ]
+          },
+
+          { text: 'External', link: 'https://google.com' },
+
+      ],
+    },
+}
